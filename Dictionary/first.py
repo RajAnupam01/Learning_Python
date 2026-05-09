f33a543a28c8ad9b1e18f27a08039720ca966a66
Anupam <anupam.r2301@gmail.com>
2026-05-09 23:48:56 +0530

    learning dictionary

---
+++ Dictionary/first.py
@@ -0,0 +1,6 @@
+my_dict = {"name": "Raj", "age": 25, "city": "Delhi"}
+print(my_dict["name"])   # Raj
+print(my_dict.get("country", "Not Found"))
+my_dict["country"] = "India"
+my_dict["age"] = 26
+
