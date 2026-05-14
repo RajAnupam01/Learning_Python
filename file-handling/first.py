7edeb3b76591283f5959e9075b010f3907edd15a
Anupam <anupam.r2301@gmail.com>
2026-05-14 22:40:19 +0530

    file handling

---
+++ file-handling/first.py
@@ -0,0 +1,13 @@
+# file handling
+
+# p = open(r'C:\Users\anupa\Documents\personal_folder\contact.txt')
+# print(p.read())
+
+# r = open("superman.txt","w")
+# r.write("This is superman from superman.txt")
+# r.close()
+
+# r = open("superman.txt","a")
+# r.write("and i am appending some content in the file")
+# r.close()
+
